a05ee347926805a874af0c22fa3274b8fdb09a1e
Allyson Stone <allysonstone@Allysons-MacBook-Air.local>
2026-03-19 19:13:03 -0500

    move firebase config to env vars

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCs0Mw8A4ZgrFDItcJRIB1DvVP9w2KXRyw",
-  authDomain: "brain-index-6a1f7.firebaseapp.com",
-  projectId: "brain-index-6a1f7",
-  storageBucket: "brain-index-6a1f7.firebasestorage.app",
-  messagingSenderId: "1040791594551",
-  appId: "1:1040791594551:web:fa5fd62f72d920b47101b2",
-  measurementId: "G-YVVL2CGHXY"
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_ID,
+  appId: import.meta.env.VITE_FB_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
