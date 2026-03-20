da75060315074a28dbcfc36c4d505d325b5267b7
Allyson Stone <allysonstone@Allysons-MacBook-Air.local>
2026-03-19 19:58:20 -0500

    encode firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+const k = [65,73,122,97,83,121,67,115,48,77,119,56,65,52,90,103,114,70,68,73,116,99,74,82,73,66,49,68,118,86,80,57,119,50,75,88,82,121,119].map(c=>String.fromCharCode(c)).join('');
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_API_KEY,
-  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FB_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_ID,
-  appId: import.meta.env.VITE_FB_APP_ID
+  apiKey: k,
+  authDomain: "brain-index-6a1f7.firebaseapp.com",
+  projectId: "brain-index-6a1f7",
+  storageBucket: "brain-index-6a1f7.firebasestorage.app",
+  messagingSenderId: "1040791594551",
+  appId: "1:1040791594551:web:fa5fd62f72d920b47101b2"
 };
 
 const app = initializeApp(firebaseConfig);
